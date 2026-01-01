dd54a3f8975faeaf7d25c75ddd22218972b7fdfd
anil-superchat <anil.g@superchat.in>
2026-01-01 15:04:43 +0530

    nothing new

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
     setLoading(true);
 
     axios
-      .post("https://portfolio-production-ae74.up.railway.app/register", {
+      .post("https://portfolio-0a84.onrender.com/register", {
         userName,
         email,
         message: msg, // FIXED
